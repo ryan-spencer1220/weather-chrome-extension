36a69b9e64b9443b7702134efc9f83071bc3d25a
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-09-10 11:23:41 -0700

    add base boilerplate files

---
+++ webpack.common.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    popup: path.resolve("src/popup/popup.tsx"),
+    options: path.resolve("src/options/options.tsx"),
+    background: path.resolve("src/background/background.ts"),
+    content: path.resolve("src/contentScript/contentScript.ts"),
+  },
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+      },
+      {
+        type: "asset/resource",
+        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf)$/i,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("src/static"),
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+    ...getHTMLPlugins(["popup", "options"]),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("dist"),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+};
+
+function getHTMLPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: "Weather Extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
